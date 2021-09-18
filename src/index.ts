191fb997bfdbc0bbe9718900b6c19ac8705227e9
hirohe <hesreg@hotmail.com>
2021-09-18 10:50:35 +0800

    refactor: export default { plugin: ... }

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
 import {TemplateInputPlugin} from './plugin';
 
-// Export your plugin as constant `plugin`
-export const plugin = TemplateInputPlugin;
+export default {plugin: TemplateInputPlugin};
